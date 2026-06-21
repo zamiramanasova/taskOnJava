6a3a2da1d43edfdd61f41ed08eeba0c6737c9a63
Zamira <keldibaevaz@gmail.com>
2026-06-21 19:55:29 +0300

    second 4

---
+++ src/Task141122/Lesson51.java
@@ -0,0 +1,12 @@
+package Task141122;
+
+import java.util.Scanner;
+
+public class Lesson51 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int number = Integer.parseInt(text);
+        System.out.println(number);
+    }
+}
