34dc5c647f0919f1f8cc3532091e51178dd53b6f
Zamira <keldibaevaz@gmail.com>
2026-06-24 12:07:38 +0300

    second 4

---
+++ src/Task141122/Lesson70.java
@@ -0,0 +1,22 @@
+package Task141122;
+
+/**
+ *
+ */
+public class Lesson70 {
+    public static void main(String[] args) {
+        String a = "Python";
+        String b = "Tutorial";
+        StringBuilder s = new StringBuilder(a + b + a);
+        System.out.println(s);
+    }
+    //правильное решение
+//    String str1 ="Python";
+//    String str2 ="Tutorial";
+//
+//	if(str1.length() >= str2.length())
+//            System.out.println( str2+str1+str2);
+//	else
+//            System.out.println(str1+str2+str1);
+
+}
